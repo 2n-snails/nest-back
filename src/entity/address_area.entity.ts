eb01b92b131b1777747c2930f2658d7a5c642297
SY <12tndbs12@naver.com>
2021-07-05 23:51:55 +0900

    파일명 업데이트

---
+++ src/entity/address_area.entity.ts
@@ -0,0 +1,27 @@
+import { AddressCity } from 'src/entity/address_city.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class AddressArea {
+  @PrimaryGeneratedColumn()
+  area_no: number;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+  })
+  area_name: string;
+
+  @Column('varchar', { name: 'deleted', length: 15, default: 'N' })
+  deleted: string;
+
+  @ManyToOne(() => AddressCity)
+  @JoinColumn({ name: 'area_city_no' })
+  area_city_no: AddressCity;
+}
