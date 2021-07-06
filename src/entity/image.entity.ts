d1147dc2423dc5beca122aa73363245fa182ae5d
SY <12tndbs12@naver.com>
2021-07-06 21:31:35 +0900

    image 엔티티 정의

--- src/entity/image.entity.ts
+++ src/entity/image.entity.ts
@@ -1,16 +1,31 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from './product.entity';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
 @Entity()
 export class Image {
   @PrimaryGeneratedColumn()
   image_no: number;
 
-  @Column('varchar', { name: 'image_src', length: 250 })
+  @Column({ type: 'varchar', length: 250 })
   image_src: string;
 
-  @Column('number', { name: 'image_order' })
+  @Column({ type: 'integer' })
   image_order: number;
 
-  @Column('varchar', { name: 'deleted', length: 15, default: 'N' })
+  @Column({
+    type: 'varchar',
+    length: 15,
+    default: 'N',
+  })
   deleted: string;
+
+  @ManyToOne(() => Product, (product) => product.images)
+  @JoinColumn({ name: 'image_product_no' })
+  product: Product;
 }
--- src/entity/product.entity.ts
+++ src/entity/product.entity.ts
@@ -2,34 +2,41 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Image } from './image.entity';
 
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn()
   product_no: number;
 
-  @Column('varchar', { name: 'product_title', length: 50 })
+  @Column({ type: 'varchar', length: 50 })
   product_title: string;
 
-  @Column('varchar', { name: 'product_content', length: 200 })
+  @Column({ type: 'varchar', length: 200 })
   product_content: string;
 
-  @Column('varchar', { name: 'product_price', length: 25 })
+  @Column({ type: 'varchar', length: 25 })
   product_price: string;
 
-  @Column('number', { name: 'product_view', default: 0 })
+  @Column({ type: 'integer', default: 0 })
   product_view: number;
 
-  @Column('boolean', { name: 'product_state', default: false })
+  @Column({ type: 'boolean', default: false })
   product_state: boolean;
-
-  @Column('varchar', { name: 'deleted', length: 15, default: 'N' })
-  deleted: string;
 
   @CreateDateColumn()
   created_at: Date;
 
-  // 관계 설정
+  @Column({
+    type: 'varchar',
+    length: 15,
+    default: 'N',
+  })
+  deleted: string;
+
+  @OneToMany(() => Image, (image) => image.product)
+  images: Image[];
 }
