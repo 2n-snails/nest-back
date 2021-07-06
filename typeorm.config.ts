14e4bdef47635d2d8cb81fa760c71cf0857f0bd9
12tndbs12 <12tndbs12@naver.com>
2021-07-06 11:30:28 +0900

    host .env로 분리

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
