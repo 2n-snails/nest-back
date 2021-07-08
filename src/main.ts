5a0262d05f1f69fa730436d2cc40671afcb2bab5
hanjuren <juren528@gmail.com>
2021-07-08 17:51:45 +0900

    docs(validation-pipe): ValidationPipe setting

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,10 +13,17 @@ async function bootstrap() {
     .setTitle('Shoppingmall API')
     .setDescription('Shoppingmall REST API')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 엔티티 데코레이터에 없는 값은 거름
+      forbidNonWhitelisted: true, // 엔티티 데코레이터에 없는 값이 들어오면 해당 값에 대한 에러 메시지 리턴
+      transform: true, // 컨트롤러가 값을 받을 때 컨트롤러에 정의한 타입으로 형 변환
+    }),
+  );
 
   await app.listen(port);
   console.log(`listening on port ${port}`);
