bb56fdba54db87adc3fcb90b8fc20cb453901cec
hanjuren <juren528@gmail.com>
2021-07-09 16:10:45 +0900

    feat(controller): app.controller define

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('main')
+  mainPageData() {
+    return 'Main Page Router';
+  }
+
+  @Get('search')
+  productSearch() {
+    return 'Search Product';
   }
 }
