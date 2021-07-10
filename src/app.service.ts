4592fd9ba53eb3cb24ecb7aadd54b0d8060e7cf1
SY <12tndbs12@naver.com>
2021-07-10 18:45:16 +0900

    feat(env): .env 파일 나누기 및 명령어 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.DB_HOST);
     return 'Hello World!';
   }
 }
