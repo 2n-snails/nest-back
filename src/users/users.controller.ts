367396a2b884acb63a5b3aa46d73e0bf49c500f0
hanjuren <juren528@gmail.com>
2021-07-11 16:38:32 +0900

    feat(user.controller): kakao login router define

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { KakaoAuthGuard } from 'src/auth/guard/kakao.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // 카카오 로그인 실행
+  @UseGuards(KakaoAuthGuard)
+  @Get('auth/kakao')
+  kakaoLogin() {
+    return process.env.KAKAO_KEY;
+  }
+  // 카카오 로그인 콜백
+  @UseGuards(KakaoAuthGuard)
+  @Get('auth/kakaocallback')
+  kakaocallback(@Res() res) {
+    // 토큰 생성 기능 추가 예정
+    res.redirect('http://localhost:4000');
+  }
+}
