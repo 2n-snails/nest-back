0f43030b2b3a4b09ec1dc42e749e63bd9726fb84
hanjuren <juren528@gmail.com>
2021-07-11 16:37:48 +0900

    feat(passport-session): passport-session setting

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { KakaoAuthGuard } from './guard/kakao.guard';
+import { PassportModule } from '@nestjs/passport';
+import { SessionSerializer } from './session.serializer';
+import { KakaoStrategy } from './strategy/kakao.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    // 패스포트 세션 설정
+    PassportModule.register({ session: true }),
+  ],
+  providers: [AuthService, KakaoStrategy, KakaoAuthGuard, SessionSerializer],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.SERVICE_PORT || 4000;
+
+  // 세션 쿠키 설정
+  app.use(cookieParser());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const config = new DocumentBuilder()
     .setTitle('Shoppingmall API')
