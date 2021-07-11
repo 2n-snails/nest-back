6517d4bdf63babb0f58b8b05612b4d2af7a51508
SY <12tndbs12@naver.com>
2021-07-11 11:56:02 +0900

    fix(env): env 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,10 +37,10 @@ import { Wish } from './entity/wish.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5432,
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DATABASE,
+      database: process.env.DB_DATABASE,
       entities: ['dist/**/*.entity{.ts,.js}'],
       migrations: [__dirname + '/src/migrations/*.ts'],
       cli: { migrationsDir: 'src/migrations' },
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.DB_HOST);
     return 'Hello World!';
   }
 }
