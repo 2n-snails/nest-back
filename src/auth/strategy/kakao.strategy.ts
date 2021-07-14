b3192be0f3299a38c190395d497f326744eb61b8
hanjuren <juren528@gmail.com>
2021-07-14 16:42:43 +0900

    docs(kakao strategy): kakao Strategy file Modify

--- src/auth/strategy/kakao.strategy.ts
+++ src/auth/strategy/kakao.strategy.ts
@@ -4,20 +4,34 @@ import { PassportStrategy } from '@nestjs/passport';
 import { AuthService } from '../auth.service';
 
 @Injectable()
-export class KakaoStrategy extends PassportStrategy(Strategy) {
+export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
   constructor(private readonly authService: AuthService) {
-    super(
-      {
-        // 카카오 전략 옵션
-        clientID: process.env.KAKAO_KEY,
-        callbackURL: 'http://localhost:4000/users/auth/kakaocallback', // 콜백 URL은 변수로 뺴야 할듯...
-      },
-      async (accessToken: any, refreshToken: any, profile: any, done: any) => {
-        // 받아온 정보를 통해 해당 유저가 있는지 체크 후 토큰 발급 및 가입 또는 로그인 처리
-        console.log(profile);
-        const user = profile;
-        done(null, user);
-      },
-    );
+    super({
+      // 카카오 전략 옵션
+      clientID: process.env.KAKAO_KEY,
+      callbackURL: `${process.env.SERVER_URL}users/auth/kakaocallback`, // 콜백 URL은 변수로 뺴야 할듯...
+    });
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: any,
+  ): Promise<any> {
+    try {
+      const user_email = profile._json.kakao_account.email;
+      const { profile_image, nickname } = profile._json.properties;
+      const user = await this.authService.validateUser(user_email);
+      if (!user) {
+        // 데이터베이스에 해당 유저의 정보가 없을 때
+        const kakaoUserData = { user_email, profile_image, nickname };
+        done(null, { kakaoUserData, loginsuccess: false });
+      } else {
+        // 해당 유저의 정보가 있을 때
+        done(null, { user, loginsuccess: true });
+      }
+    } catch (error) {
+      return error;
+    }
   }
 }
