2f5d042fd51310bd2b610e5f038996e8df6b501c
hanjuren <juren528@gmail.com>
2021-07-14 16:39:44 +0900

    feat(default strategy cahnge): from session to jwt-token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,21 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { KakaoAuthGuard } from './guard/kakao.guard';
 import { PassportModule } from '@nestjs/passport';
-import { SessionSerializer } from './session.serializer';
-import { KakaoStrategy } from './strategy/kakao.strategy';
+import { KakaoStrategy } from 'src/auth/strategy/kakao.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
 
 @Module({
   imports: [
     // 패스포트 세션 설정
-    PassportModule.register({ session: true }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    forwardRef(() => UsersModule),
+    JwtModule.register({
+      secret: String(process.env.JWT_SECRET_KEY),
+    }),
   ],
-  providers: [AuthService, KakaoStrategy, KakaoAuthGuard, SessionSerializer],
+  providers: [AuthService, KakaoStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
