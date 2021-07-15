759289d99d17495fa85473756effea28bc9ffd8a
12tndbs12 <12tndbs12@naver.com>
2021-07-15 17:53:45 +0900

    feat(auth): validateUser 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [UsersModule],
   providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(user_email: string): Promise<any> {
+    const user = await this.usersService.findOne(user_email);
+    if (user) {
+      // 유저가 있으면 로그인 성공: jwt 토큰 발급
+      return '성공!';
+    }
+    // 유저가 없으면 추가정보 필요: 프론트로 1회용 jwt 토큰 발급
+    return '실패!';
+  }
+}
