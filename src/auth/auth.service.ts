1e79f928e43357f97c7f04523672df9c6fb96580
hanjuren <juren528@gmail.com>
2021-07-17 00:11:27 +0900

    feat(jwt-token): Token generation when Kakao login fails

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(user_email: string): Promise<any> {
+    const user = this.userService.findOneEmail(user_email);
+    if (user) {
+      return user;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    return '로그인 후 토큰 발급 해줘야 합니다.';
+  }
+
+  async registUserData(user: any) {
+    // userData가 kakao에서 넘어온 데이터 일때
+    if (user.userData.type === 'kakao') {
+      const { user_email, profile_image, nickname } = user.userData;
+      const payload = {
+        user_email,
+        user_profile_image: profile_image,
+        user_nick: nickname,
+      };
+      return {
+        access_token: this.jwtService.sign(payload, {
+          subject: 'user-data',
+          expiresIn: '10m',
+        }),
+      };
+    } else {
+      // 네이버 일때
+      return;
+    }
+    // 두개를 나누는 이유는 카카오는 전화번호 정보를 받지 못하기 때문에 payload가 다르기 때문입니다.
+  }
+}
