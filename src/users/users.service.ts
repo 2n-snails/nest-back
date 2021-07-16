11a5478d8fd5a8571366bfac736e486b0a7e88fb
hanjuren <juren528@gmail.com>
2021-07-16 23:58:19 +0900

    feat(User Service): create find user function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findOneEmail(user_email: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { user_email } });
+  }
+}
