08eb2fcbb1ef09f1662e68ba7e2860c05fa8e427
hanjuren <juren528@gmail.com>
2021-07-16 23:51:48 +0900

    pull main branch

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,70 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  // 상품 업로드
+  @Post('upload')
+  productUpload() {
+    return 'product upload';
+  }
+
+  // 상품 상세 정보
+  @Get(':product-id')
+  productInfo() {
+    return 'product info';
+  }
+
+  // 추천 상품
+  @Get(':product-id/recommend')
+  recommendProduct() {
+    return 'recommend products';
+  }
+
+  // 상품 댓글 작성
+  @Post(':product-id/comment')
+  writeComment() {
+    return 'write Comment';
+  }
+
+  // 대댓글 작성
+  @Post(':product-id/recomment')
+  writeReComment() {
+    return 'write ReComment';
+  }
+
+  // 신고하기
+  @Post(':product-id/report')
+  declareProduct() {
+    return 'declare a product';
+  }
+
+  // 찜하기
+  @Post(':product-id/wish')
+  wishProduct() {
+    return 'wish product';
+  }
+
+  // 판매자 번호 보내주기
+  @Post(':product-id/seller-num')
+  sendPhoneNumber() {
+    return 'send phone number';
+  }
+
+  // 상품 수정
+  @Put(':product-id/update')
+  productInfoUpdate() {
+    return 'product update';
+  }
+
+  // 상품 삭제
+  @Delete(':product-id')
+  deletedProduct() {
+    return 'delete product';
+  }
+
+  // 찜 취소하기
+  @Delete(':product-id/wish')
+  wishCancleProduct() {
+    return 'wish product';
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -16,9 +15,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return;
   });
 });
