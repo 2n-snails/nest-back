741ab53e982ba79396bc4471216fe990c87aea9f
hanjuren <juren528@gmail.com>
2021-07-17 00:05:51 +0900

    feat(user Controller): Token issuance function according to the return value

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,39 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
-import { KakaoAuthGuard } from 'src/auth/guard/kakao.guard';
+import {
+  Controller,
+  forwardRef,
+  Get,
+  Inject,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
   // 카카오 로그인 실행
-  @UseGuards(KakaoAuthGuard)
+  @UseGuards(AuthGuard('kakao'))
   @Get('auth/kakao')
-  kakaoLogin() {
-    return process.env.KAKAO_KEY;
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async kakaoLogin() {}
   // 카카오 로그인 콜백
-  @UseGuards(KakaoAuthGuard)
+  @UseGuards(AuthGuard('kakao'))
   @Get('auth/kakaocallback')
-  kakaocallback(@Res() res) {
-    // 토큰 생성 기능 추가 예정
-    res.redirect('http://localhost:4000');
+  async kakaocallback(@Req() req) {
+    if (req.user.loginsuccess) {
+      // 로그인 성공시 토큰 발급
+      return this.authService.login(req.user);
+    } else {
+      // 로그인 실패시 소셜 데이터를 담은 토큰 전달
+      const userData = await this.authService.registUserData(req.user);
+      return {
+        userData,
+        loginsuccess: false,
+      };
+    }
   }
 }
