849a0ba46970587fa847b223b40674ef013009e6
SY <12tndbs12@naver.com>
2021-07-20 00:56:18 +0900

    feat(jwt): jwt 토큰 헤더로 보내주기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
+import { Response } from 'express';
 import { AuthService } from '../auth/auth.service';
 import { NaverAuthGuard } from '../auth/guard/naver-auth.guard';
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -14,9 +15,17 @@ export class UsersController {
 
   @UseGuards(NaverAuthGuard)
   @Get('auth/naver/callback')
-  async callback(@Request() req): Promise<any> {
+  async callback(@Req() req, @Res() res: Response): Promise<any> {
     // 최종적으로 프론트로 보내주는 부분
-    return req.user;
-    // return this.authService.login(req.user);
+    console.log(req.user.access_token);
+    res.header('token', req.user.access_token);
+    res.send(req.user);
+    res.end();
   }
+
+  // @Post('auth/login')
+  // userLogin(@Req req: ) {
+
+  //   // 타입ORM
+  // }
 }
