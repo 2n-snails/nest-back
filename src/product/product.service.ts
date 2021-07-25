61696e29a548f91fdda1fdd92904a82edaefb4c3
SY <12tndbs12@naver.com>
2021-07-25 20:10:05 +0900

    상품 상세 정보 쿼리 작성중

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entity/product.entity';
+import { getRepository } from 'typeorm';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  async getOne(product_no: number) {
+    const product = await getRepository(Product)
+      .createQueryBuilder('product')
+      .where('product.product_no = :product_no', { product_no })
+      .leftJoinAndSelect('product.images', 'image')
+      .leftJoinAndSelect('product.productCategories', 'ProductCategory')
+      .getMany();
+
+    return product;
+  }
+}
