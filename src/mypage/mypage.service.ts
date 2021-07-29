3824781695bb2abf94c489a1101a9e35e0cfde3e
hanjuren <juren528@gmail.com>
2021-07-29 15:03:38 +0900

    feat(mypage Controller): user nickname update

--- src/mypage/mypage.service.ts
+++ src/mypage/mypage.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entity/user.entity';
+import { getRepository } from 'typeorm';
 
 @Injectable()
-export class MypageService {}
+export class MypageService {
+  // 유저 닉네임 수정하기
+  async userNickUpdate(data, user_no) {
+    const { user_nick } = data;
+    try {
+      const result = await getRepository(User)
+        .createQueryBuilder()
+        .update(User, { user_nick: user_nick })
+        .returning('*')
+        .where(`user_no = ${user_no}`)
+        .updateEntity(true)
+        .execute();
+      if (result.affected > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+}
