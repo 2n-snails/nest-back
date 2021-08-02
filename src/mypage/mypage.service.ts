ac8730064e28cd22d7f5427d3c7f4d5e1acff727
SY <12tndbs12@naver.com>
2021-08-02 15:04:55 +0900

    feat(my_wish): 유저의 찜 목록 완료

--- src/mypage/mypage.service.ts
+++ src/mypage/mypage.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/entity/user.entity';
-import { Wish } from 'src/entity/wish.entity';
 import { getRepository } from 'typeorm';
 
 @Injectable()
@@ -27,28 +26,37 @@ export class MypageService {
   }
   // 유저 찜 목록
   async findUserWish(user_id) {
-    const result = await getRepository(Wish)
-      .createQueryBuilder('wish')
-      .leftJoinAndSelect('wish.user', 'u')
+    const result = await getRepository(User)
+      .createQueryBuilder('u')
+      .leftJoinAndSelect('u.wishes', 'wish')
       .leftJoinAndSelect('wish.product', 'p')
       .leftJoinAndSelect('p.images', 'image')
-      .leftJoinAndSelect('p.productCategories', 'category')
+      .leftJoinAndSelect('p.comments', 'comment')
+      .leftJoinAndSelect('p.productCategories', 'productCategory')
+      .leftJoinAndSelect('productCategory.category', 'category')
+      .leftJoinAndSelect('p.user', 'seller')
+      .leftJoinAndSelect('seller.deals', 'deal')
+      .leftJoinAndSelect('deal.addressArea', 'addressArea')
+      .leftJoinAndSelect('addressArea.addressCity', 'addressCity')
       .select([
+        'u.user_no',
         'wish.wish_no',
-        // 상품 정보
         'p.product_no',
         'p.product_title',
-        'p.product_price',
-        // 상품 이미지
+        'p.product_content',
+        'p.product_view',
+        'p.product_state',
+        'p.createdAt',
+        'productCategory.product_category_no',
+        'category.category_name',
         'image.image_src',
-        'image_order',
+        'image.image_order',
+        'seller.user_no',
+        'seller.user_nick',
+        'deal.deal_no',
+        'addressArea.area_name',
+        'addressCity.city_name',
       ])
-      .loadRelationCountAndMap(
-        'p.wishCount',
-        'p.wishes',
-        'productWishCount',
-        (qb) => qb.where(`productWishCount.deleted = 'N'`),
-      )
       .loadRelationCountAndMap(
         'p.commentCount',
         'p.comments',
@@ -56,12 +64,19 @@ export class MypageService {
         (qb) => qb.where(`commentCount.deleted = 'N'`),
       )
       .loadRelationCountAndMap(
-        'u.wishCount',
+        'p.productWishCount',
+        'p.wishes',
+        'productWish',
+        (qb) => qb.where(`productWish.deleted = 'N'`),
+      )
+      .loadRelationCountAndMap(
+        'u.userWishCount',
         'u.wishes',
         'userWishCount',
         (qb) => qb.where(`userWishCount.deleted = 'N'`),
       )
-      .where(`wish.user = ${user_id}`)
+      .where(`u.user_no = ${user_id}`)
+      .andWhere(`wish.deleted = 'N'`)
       .getMany();
     return result;
   }
