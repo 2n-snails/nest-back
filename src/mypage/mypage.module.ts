f64a73688ac727d2f080fea34f6ec1eca9868803
hanjuren <juren528@gmail.com>
2021-08-11 16:25:28 +0900

    docs(module): imports AuthModule

--- src/mypage/mypage.module.ts
+++ src/mypage/mypage.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MypageService } from './mypage.service';
 import { MypageController } from './mypage.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [AuthModule, UsersModule],
   providers: [MypageService],
   controllers: [MypageController],
 })
