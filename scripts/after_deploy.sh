2023da8991b49a274c6daa3ab1f05eefd3c60cdd
SY <12tndbs12@naver.com>
2021-08-21 01:11:16 +0900

    환경변수 추가

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,4 +2,8 @@
 REPOSITORY=/home/ec2-user/nest-back
 cd $REPOSITORY
 
+# nvm 환경변수 등록
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nohup npm run start:dev
