bb3a0081bd91e28e1f22315b22f65d845db0f94b
SY <12tndbs12@naver.com>
2021-08-21 20:55:45 +0900

    경로 수정

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -3,10 +3,12 @@ REPOSITORY=/home/ec2-user/nest-back
 cd $REPOSITORY
 
 # nvm 환경변수 등록
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 cd secret
 mv .env.development ../
 cd ..
+
 nohup npm run start:dev &
