6843a8f41ab9fbf2f5e87e2a49be8a3b84bc6341
SY <12tndbs12@naver.com>
2021-08-21 22:22:09 +0900

    fix(script): 권한 추가

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -3,5 +3,5 @@ REPOSITORY=/home/ec2-user
 cd $REPOSITORY
 sudo su
 
-kill -9 `ps -ef|grep nest|awk '{print $2}'`
-rm -rf nest-back
+sudo kill -9 `ps -ef|grep nest|awk '{print $2}'`
+sudo rm -rf nest-back
