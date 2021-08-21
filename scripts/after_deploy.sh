bac5c83be22568fe03392da5d6c8b2e1d1680368
SY <12tndbs12@naver.com>
2021-08-21 20:10:22 +0900

    .env 추가

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -6,4 +6,7 @@ cd $REPOSITORY
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+cd secret
+mv .env.development ../
+cd ..
 nohup npm run start:dev
