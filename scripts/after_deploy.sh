14565493986a23a8f42a4d19594aa9df234a3faa
SY <12tndbs12@naver.com>
2021-08-22 16:24:04 +0900

    sudo su 삭제

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,8 +2,6 @@
 REPOSITORY=/home/ec2-user/nest-back
 cd $REPOSITORY
 
-sudo su
-
 # nvm 환경변수 등록
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 REPOSITORY=/home/ec2-user
 cd $REPOSITORY
-sudo su
 
 sudo kill -9 `ps -ef|grep nest|awk '{print $2}'`
 sudo rm -rf nest-back
