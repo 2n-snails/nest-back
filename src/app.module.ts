0851906bfec6b1ad8efba3869f93a386d86dd0dd
SY <12tndbs12@naver.com>
2021-08-26 22:20:15 +0900

    feat(sentry): providers 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ProductCategory } from './entity/product_category.entity';
 import { AddressCity } from 'src/entity/address_city.entity';
 import { AddressArea } from './entity/address_area.entity';
@@ -26,7 +27,7 @@ import { Product } from './entity/product.entity';
 import { ReComment } from './entity/recomment.entity';
 import { Review } from './entity/review.entity';
 import { Wish } from './entity/wish.entity';
-import { RavenModule } from 'nest-raven';
+import { RavenModule, RavenInterceptor } from 'nest-raven';
 
 @Module({
   imports: [
@@ -77,7 +78,13 @@ import { RavenModule } from 'nest-raven';
     RavenModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useValue: new RavenInterceptor(),
+    },
+    AppService,
+  ],
   exports: [AppService],
 })
 export class AppModule implements NestModule {
